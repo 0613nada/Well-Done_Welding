3d9dc3193d2a45a6120c1b01f867cdc7d99a44dc
sw0821 <na020821@gmail.com>
2024-07-27 21:41:21 +0900

    24/07/27
    캐릭터 마우스커서에 따라 방향전환 가능하게 구현

--- Well-Done_Welding/Assets/Code/PlayerCode/Player.cs
+++ Well-Done_Welding/Assets/Code/PlayerCode/Player.cs
@@ -1,24 +1,31 @@
 using Febucci.UI.Actions;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     // 변수선언
 
-
-    public Vector2 inputVec;
+    Vector2 mousePos;
+    Vector2 inputVec;
     Rigidbody2D rigid;
     SpriteRenderer spriter;
     Animator animator;
-
+    Camera camera;
     // 플레이어 속도
-    public float speed;
+    public float PlayerSpeed;
 
+
+    private void Start()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
     private void Awake()
     {
         // 값 초기화
+        PlayerSpeed = 100;
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
@@ -26,6 +33,9 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        // 마우스커서 인게임 인식
+        mousePos = Input.mousePosition;
+        mousePos = camera.ScreenToWorldPoint(mousePos);
         // 벡터값을 인풋:Horizontal, Vertical값으로 입력받음
         inputVec.x = Input.GetAxisRaw("Horizontal");
         inputVec.y = Input.GetAxisRaw("Vertical");
@@ -34,7 +44,7 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         // 플레이어 이동
-        rigid.velocity = inputVec.normalized * speed;
+        rigid.velocity = (inputVec.normalized * 2.5f) * (PlayerSpeed / 100);
     }
 
     private void LateUpdate()
@@ -42,11 +52,10 @@ public class Player : MonoBehaviour
         // 애니매이션 움직임 구현
         animator.SetFloat("Speed", inputVec.magnitude);
 
-
-        if (inputVec.x != 0)
-        {
-            // 캐릭터 방향전환
-            spriter.flipX = inputVec.x < 0;
-        }
+        // 마우스 위치에 따라 캐릭터 방향 변경
+        spriter.flipX = mousePos.x < transform.position.x;
+        
     }
+
+    
 }
